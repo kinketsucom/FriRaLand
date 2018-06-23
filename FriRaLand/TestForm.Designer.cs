877c9c9dcd46bd0fa4475a60a09e0f35b7923765
kinketsucom <sekibotbot@gmail.com>
2018-06-23 13:22:35 +0900

    ラクマのいらんやつ全削除、ビルドできるようになりました

--- FriRaLand/TestForm.Designer.cs
+++ FriRaLand/TestForm.Designer.cs
@@ -25,14 +25,13 @@
         private void InitializeComponent() {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(33, 75);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(299, 22);
             this.textBox1.TabIndex = 0;
@@ -40,26 +39,15 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(33, 122);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(291, 22);
             this.textBox2.TabIndex = 1;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(33, 210);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 29);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "rakuma";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(631, 291);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 29);
             this.button2.TabIndex = 3;
@@ -73,10 +61,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1361, 611);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TestForm";
             this.Text = "TestForm";
             this.Load += new System.EventHandler(this.TestForm_Load);
@@ -89,7 +76,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
--- FriRaLand/TestForm.cs
+++ FriRaLand/TestForm.cs
@@ -18,18 +18,6 @@ namespace FriRaLand {
         public static string pass = "";
 
         private void button1_Click(object sender, EventArgs e) {
-            api.tryRakumaLogin();
-            item.brandId = 1573;
-            item.categoryId = new int[3]{281, 32, 3};
-            item.conditionType = 1;
-            item.deliveryMethod = 1;
-            item.deliveryTerm = 3;
-            item.descriptionText = "商品説明";
-            item.prefectureCode = 27;
-            item.productName = "商品名";
-            item.sellingPrice = 9999;
-            item.imagepaths[0] = @"画像パス";
-            api.Sell(item);
         }
 
         private void TestForm_Load(object sender, EventArgs e) {
