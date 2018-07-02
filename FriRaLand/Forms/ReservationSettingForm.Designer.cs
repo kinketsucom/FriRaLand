cb6651ac8576f118183addadddb9c6ac94cd275c
kinketsucom <sekibotbot@gmail.com>
2018-07-02 22:00:17 +0900

    連続予約機能できた

--- FriRaLand/Forms/ReservationSettingForm.Designer.cs
+++ FriRaLand/Forms/ReservationSettingForm.Designer.cs
@@ -73,6 +73,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.registerReservationButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox2.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.autoDeleteDayNumeric2)).BeginInit();
@@ -90,6 +91,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.exhibitMinuteNumeric)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.exhibitHourNumeric)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.itemPictureBox)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
@@ -417,10 +419,10 @@
             // 
             // exhibitTimeNumeric
             // 
-            this.exhibitTimeNumeric.Location = new System.Drawing.Point(185, 337);
+            this.exhibitTimeNumeric.Location = new System.Drawing.Point(147, 55);
             this.exhibitTimeNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.exhibitTimeNumeric.Name = "exhibitTimeNumeric";
-            this.exhibitTimeNumeric.Size = new System.Drawing.Size(57, 22);
+            this.exhibitTimeNumeric.Size = new System.Drawing.Size(57, 30);
             this.exhibitTimeNumeric.TabIndex = 70;
             // 
             // accountComboBox
@@ -438,7 +440,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label16.Location = new System.Drawing.Point(458, 297);
+            this.label16.Location = new System.Drawing.Point(420, 15);
             this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(29, 25);
@@ -449,7 +451,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label17.Location = new System.Drawing.Point(340, 300);
+            this.label17.Location = new System.Drawing.Point(302, 18);
             this.label17.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(46, 25);
@@ -460,7 +462,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label18.Location = new System.Drawing.Point(241, 300);
+            this.label18.Location = new System.Drawing.Point(203, 18);
             this.label18.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(29, 25);
@@ -469,33 +471,33 @@
             // 
             // intervalMinuteNumeric
             // 
-            this.intervalMinuteNumeric.Location = new System.Drawing.Point(394, 300);
+            this.intervalMinuteNumeric.Location = new System.Drawing.Point(356, 18);
             this.intervalMinuteNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.intervalMinuteNumeric.Name = "intervalMinuteNumeric";
-            this.intervalMinuteNumeric.Size = new System.Drawing.Size(57, 22);
+            this.intervalMinuteNumeric.Size = new System.Drawing.Size(57, 30);
             this.intervalMinuteNumeric.TabIndex = 65;
             // 
             // intervalHourNumeric
             // 
-            this.intervalHourNumeric.Location = new System.Drawing.Point(274, 300);
+            this.intervalHourNumeric.Location = new System.Drawing.Point(236, 18);
             this.intervalHourNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.intervalHourNumeric.Name = "intervalHourNumeric";
-            this.intervalHourNumeric.Size = new System.Drawing.Size(57, 22);
+            this.intervalHourNumeric.Size = new System.Drawing.Size(57, 30);
             this.intervalHourNumeric.TabIndex = 64;
             // 
             // intervalDayNumeric
             // 
-            this.intervalDayNumeric.Location = new System.Drawing.Point(185, 300);
+            this.intervalDayNumeric.Location = new System.Drawing.Point(147, 18);
             this.intervalDayNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.intervalDayNumeric.Name = "intervalDayNumeric";
-            this.intervalDayNumeric.Size = new System.Drawing.Size(57, 22);
+            this.intervalDayNumeric.Size = new System.Drawing.Size(57, 30);
             this.intervalDayNumeric.TabIndex = 63;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label12.Location = new System.Drawing.Point(45, 300);
+            this.label12.Location = new System.Drawing.Point(7, 18);
             this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(80, 25);
@@ -506,7 +508,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label11.Location = new System.Drawing.Point(513, 255);
+            this.label11.Location = new System.Drawing.Point(513, 238);
             this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(29, 25);
@@ -517,7 +519,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label10.Location = new System.Drawing.Point(410, 256);
+            this.label10.Location = new System.Drawing.Point(410, 239);
             this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(29, 25);
@@ -526,7 +528,7 @@
             // 
             // exhibitMinuteNumeric
             // 
-            this.exhibitMinuteNumeric.Location = new System.Drawing.Point(448, 256);
+            this.exhibitMinuteNumeric.Location = new System.Drawing.Point(448, 239);
             this.exhibitMinuteNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.exhibitMinuteNumeric.Name = "exhibitMinuteNumeric";
             this.exhibitMinuteNumeric.Size = new System.Drawing.Size(57, 22);
@@ -534,7 +536,7 @@
             // 
             // exhibitHourNumeric
             // 
-            this.exhibitHourNumeric.Location = new System.Drawing.Point(354, 256);
+            this.exhibitHourNumeric.Location = new System.Drawing.Point(354, 239);
             this.exhibitHourNumeric.Margin = new System.Windows.Forms.Padding(4);
             this.exhibitHourNumeric.Name = "exhibitHourNumeric";
             this.exhibitHourNumeric.Size = new System.Drawing.Size(57, 22);
@@ -543,7 +545,7 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(185, 256);
+            this.dateTimePicker1.Location = new System.Drawing.Point(185, 239);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(160, 22);
@@ -574,7 +576,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(45, 336);
+            this.label5.Location = new System.Drawing.Point(7, 54);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 25);
@@ -585,7 +587,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(45, 256);
+            this.label4.Location = new System.Drawing.Point(45, 239);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(80, 25);
@@ -637,25 +639,37 @@
             this.registerReservationButton.UseVisualStyleBackColor = true;
             this.registerReservationButton.Click += new System.EventHandler(this.registerReservationButton_Click);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.intervalDayNumeric);
+            this.groupBox3.Controls.Add(this.intervalHourNumeric);
+            this.groupBox3.Controls.Add(this.intervalMinuteNumeric);
+            this.groupBox3.Controls.Add(this.label18);
+            this.groupBox3.Controls.Add(this.exhibitTimeNumeric);
+            this.groupBox3.Controls.Add(this.label17);
+            this.groupBox3.Controls.Add(this.label16);
+            this.groupBox3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox3.Location = new System.Drawing.Point(42, 285);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(482, 90);
+            this.groupBox3.TabIndex = 76;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "繰り返し出品";
+            // 
             // ReservationSettingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(859, 612);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.registerReservationButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.reExhibitCheckBox1);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.exhibitTimeNumeric);
             this.Controls.Add(this.accountComboBox);
-            this.Controls.Add(this.label16);
-            this.Controls.Add(this.label17);
-            this.Controls.Add(this.label18);
-            this.Controls.Add(this.intervalMinuteNumeric);
-            this.Controls.Add(this.intervalHourNumeric);
-            this.Controls.Add(this.intervalDayNumeric);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.exhibitMinuteNumeric);
@@ -663,7 +677,6 @@
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.itemNameLabel);
             this.Controls.Add(this.itemPictureBox);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -693,6 +706,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.exhibitMinuteNumeric)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.exhibitHourNumeric)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.itemPictureBox)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -750,5 +765,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button registerReservationButton;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
